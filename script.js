4d20c5a93215cee6c5314846e3c26624373ce596
Eric Del Guerra <eric.del-guerra@Digital-Grenoble.local>
2019-06-27 10:06:58 +0200

    Add commentaries

--- script.js
+++ script.js
@@ -4,186 +4,127 @@ const BAS = "B";
 const DROITE = "D";
 const GAUCHE = "G";
 
-// nombre de cases par côtégit
-var side = 4;
+// nombre de cases par côté
+let side = 4;
+
+// On utilise turn pour créer notre condition de victoire
+let turn = 0;
 
 // changement de style css en fonction de "side"
 document.documentElement.style.setProperty("--side", side);
 
 
-
 // retient l'état courant du taquin
-var current_state = [];
-var winning_state = [];
-var turn = 0;
+let current_state = [];
+let winning_state = [];
+
 // position de la case vide
-const empty_cell = { i: 0, j: 0 };
-
+const empty_cell = {i: 0, j: 0};
 // Initialisation de l'état courant
+
+
+
 function setInitState() {
-  current_state = [];    // on vide le tableau
-  var l = side;           // l = nombre de cases par côté
-  for (var i = 0; i < l; i++) {
-    current_state[i] = [];
-    for (var j = 0; j < l; j++) {
-      if (i == l - 1 && j == l - 1) {
-        val = 0;
-      } else {
-        val = i * l + j + 1;
-      }
-      current_state[i][j] = val;
+    current_state = [];     // on vide le tableau
+    let l = side;           // l = nombre de cases par côté
+    for (let i = 0; i < l; i++) {
+        current_state[i] = [];
+        for (let j = 0; j < l; j++) {
+            if (i === l - 1 && j === l - 1) {
+                val = 0;
+            } else {
+                val = i * l + j + 1;
+            }
+            current_state[i][j] = val;
+
+        }
     }
-  }
-  empty_cell.i = side - 1;
-  empty_cell.j = side - 1;
-
-  if (turn == 0){
-    for (var i = 0; i < current_state.length; i++){
-      winning_state[i] = current_state[i].slice(0);
-      turn ++;
+    if (turn === 0){
+        for(let i = 0; i < current_state.length; i++){
+            winning_state[i] = current_state[i].slice(0); // On copie le tableau pour la methode checkWin
+        }
+        turn ++;
     }
-  }
-  
 
+    console.log(" WIN " + winning_state)
+
+    empty_cell.i = side - 1;
+    empty_cell.j = side - 1;
 }
 
-
-
-function applyMove(ec, move) {
-
-  let futurPos = { i: ec.i, j: ec.j }
-  switch (move) {
-    case HAUT:
-      futurPos.i--;
-      movePos(futurPos, ec);
-      break;
-    case BAS:
-      futurPos.i++;
-      console.log(ec)
-      movePos(futurPos, ec);
-      break;
-    case GAUCHE:
-      futurPos.j--
-      console.log(ec)
-      movePos(futurPos, ec);
-      break;
-    case DROITE:
-      futurPos.j++;
-      console.log(ec)
-      movePos(futurPos, ec);
-      break;
-  }
-}
-
-
-function movePos(futurPos, ec) {
-  
-  if (futurPos.i < current_state.length && futurPos.j < current_state.length 
-     && futurPos.i >= 0 && futurPos.j >= 0 ) {
-    let oldValue = current_state[futurPos.i][futurPos.j]
-    current_state[futurPos.i][futurPos.j] = 0
-    current_state[ec.i][ec.j] = oldValue
-    ec.i = futurPos.i;
-    ec.j = futurPos.j;
-  }
-}
-
-
+// Methode d'affichage pour le visuel
 function displayState(tab) {
-  $(".grid").empty();
-  for (let i = 0; i < tab.length; i++) {
-    for (let j = 0; j < tab[i].length; j++) {
-      const elem = tab[i][j];
-      if (elem) {
-        const item = $(
-          `<div data-i="${i}" data-j="${j}" class="item" id="${elem}">${elem}</div>`
-        );
-        $(".grid").append(item);
-      } else {
-        $(".grid").append(`<div class="vide"></div>`);
-      }
+    $(".grid").empty();
+    for (let i = 0; i < tab.length; i++) {
+        for (let j = 0; j < tab[i].length; j++) {
+            const elem = tab[i][j];
+            if (elem) {
+                const item = $(`<div data-i="${i}" data-j="${j}" class="item" id="${elem}">${elem}</div>`);
+                $(".grid").append(item);
+            } else {
+                $(".grid").append(`<div class="vide""></div>`);
+            }
+
+        }
     }
-  }
+
+
 }
 
-
-function doRandomShuffle (current_state, empty_cell) {
-  current_state.sort(() => Math.random() - 0.5);
-  
-  for (let i = 0; i < current_state.length; i++){
-    current_state[i].sort(() => Math.random() - 0.5);
-  }
-  
-  for (let i = 0; i < current_state.length; i++) {
-    for (let j = 0; j < current_state[i].length; j++) {
-      if (current_state[i][j] === 0){
-        empty_cell.i = i;
-        empty_cell.j = j;
-      }
-      
-    }
-  }
-  console.log("current =" + current_state)
-  console.log("winning =" + winning_state)
-}
-
-
-
+// Au clique sur "Check" on verifie si les positions sont bonnes
 $(".check").click(function () {
-  if (checkWin()) {
-    displayWin();
-  }
-  // TODO: penser à implémenter la fonction checkWin
+    console.log("Is winning? ", checkWin());
+    if (checkWin()) {
+        displayWin();
+    }
 });
 
 
 $(".reset").click(reset);
 
 $(".shuffle").click(function () {
-  // pas le temps de faire le shuffle
-  doRandomShuffle(current_state, empty_cell);
-  displayState(current_state);
+    // pas le temps de faire le shuffle
+    doRandomShuffle(current_state, empty_cell);
+    displayState(current_state);
 });
 
 $(".solution").click(function () {
-  console.log("Solution demandée par l'utilisateur·ice")
-  findSolution(current_state, empty_cell);
+    console.log("Solution demandée par l'utilisateur·ice")
+    findSolution(current_state, empty_cell);
 });
 
 
 // Pour augmenter / diminuer la taille d'un côté.
 $(".plus").click(function () {
-  document.documentElement.style.setProperty("--side", ++side);
-  turn = 0;
-  reset();
-  console.log("Plus grand")
+    turn = 0
+    document.documentElement.style.setProperty("--side", ++side);
+    reset();
+    console.log("Plus grand")
 });
 
 $(".minus").click(function () {
-  document.documentElement.style.setProperty("--side", --side);
-  turn = 0;
-  reset();
-  console.log("Plus petit")
+    turn = 0
+    document.documentElement.style.setProperty("--side", --side);
+    reset();
+    console.log("Plus petit")
 });
 
 
-
-
-// Ici on gere l'ajout dynamique de .item 
+// Ici on gere l'ajout dynamique de .item
 $(".grid").on('click', '.item', function () {
-  console.log("J'existe et resisterai à ma mort dans un reset/ shuffle ",
-    "Valeur:", $(this).html(),
-    "Position i:", $(this).attr("data-i"),
-    "Position j:", $(this).attr("data-j"),
-    "ID:", $(this).attr("id")
-  )
+    console.log("J'existe et resisterai à ma mort dans un reset/ shuffle ",
+        "Valeur:", $(this).html(),
+        "Position i:", $(this).attr("data-i"),
+        "Position j:", $(this).attr("data-j"),
+        $(this).attr("id")
+    )
 });
 
-// Avec le code ci-dessous, j'ai des problèmes à chaque reset car les item sont 
+// Avec le code ci-dessous, j'ai des problèmes à chaque reset car les item sont
 // supprimés.
-// Pas de gestion dynamique de .item 
+// Pas de gestion dynamique de .item
 // $(".item").click(function(){
-//   console.log("Je n'existe que jusqu'à ma mort dans un reset/ shuffle")   
+//   console.log("Je n'existe que jusqu'à ma mort dans un reset/ shuffle")
 //
 
 
@@ -195,19 +136,19 @@ var span = document.getElementsByClassName("close")[0];
 
 // Pour afficher la fenetre quand on a gagné, appeler cette fonction
 function displayWin() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // Quand on clique sur <span> (x), on ferme
 span.onclick = function () {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // On ferme aussi si on clique n'importe où
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 }
 
 
@@ -215,46 +156,96 @@ window.onclick = function (event) {
 document.onkeydown = checkKey;
 
 function checkKey(e) {
-  e = e || window.event;
+    e = e || window.event;
 
-  if (e.keyCode == 38) {
-    // up arrow
-    applyMove(empty_cell, HAUT);
-  }
-  else if (e.keyCode == 40) {
-    // down arrow
-    applyMove(empty_cell, BAS);
-  }
-  else if (e.keyCode == 37) {
-    // left arrow
-    applyMove(empty_cell, GAUCHE);
-  }
-  else if (e.keyCode == 39) {
-    // right arrow
-    applyMove(empty_cell, DROITE);
-  }
-  displayState(current_state);
-  if (checkWin()) {
-    displayWin();
-  }
+    if (e.keyCode === 38) {
+        console.log("Haut")
+        // up arrow
+        applyMove(current_state, empty_cell, HAUT);
+    } else if (e.keyCode === 40) {
+        console.log("Bas")
+        // down arrow
+        applyMove(current_state, empty_cell, BAS);
+    } else if (e.keyCode === 37) {
+        // left arrow
+        applyMove(current_state, empty_cell, GAUCHE);
+    } else if (e.keyCode === 39) {
+        // right arrow
+        applyMove(current_state, empty_cell, DROITE);
+    }
+    displayState(current_state);
+    if (checkWin()) {
+        displayWin();
+    }
 }
 
-  // parcour les tableaux et verifie pour chaque index si les valeurs sont égales
-function checkWin() {
-  for (let i = 0; i < current_state.length; i++){
-    for (let j = 0; j < current_state.length; j++){
-      if (current_state[i][j] !== winning_state[i][j]){
-        return false;
-      }
+function doRandomShuffle(current_state, empty_cell) {
+    current_state.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < current_state.length; i++) { // On mélange le tableau
+        current_state[i].sort(() => Math.random() - 0.5);
     }
-  }
-  return true;
+    for (let i = 0; i < current_state.length; i++) {
+        for (let j = 0; j < current_state[i].length; j++) {
+            if (current_state[i][j] === 0){  // On cherche où notre case vide se place
+                empty_cell.i = i;
+                empty_cell.j = j;
+            }           // Puis on lui attribut les valeur
+        }
+    }
+    console.log("Tableau mélangé")
+    console.log(" WIN " + winning_state)
+}
+
+
+function applyMove(state, ec, move) {
+    let futurPos = {i: ec.i, j: ec.j} // On garde notre position pour les prochaines verifs et avant de modifier notre position actuelle
+    switch (move) {
+        case HAUT :
+            futurPos.i--;
+            movePos(futurPos, ec)
+            break
+        case BAS :
+            futurPos.i++;
+            movePos(futurPos, ec)
+            break
+        case DROITE :
+            futurPos.j++;
+            movePos(futurPos, ec)
+            break
+        case GAUCHE :
+            futurPos.j--;
+            movePos(futurPos, ec)
+            break
+    }
+}
+
+
+function checkWin() {
+    for (let i = 0; i < current_state.length; i++) {
+        for (let j = 0; j < current_state[i].length; j++) {  // On verifie chaque case et on les compare
+            if (current_state[i][j] !== winning_state[i][j]) {
+                return false // Si au moins une case est fausse la victoire est fausse
+            }
+        }
+    }
+    return true;
+}
+
+function movePos(futurPos, ec) {
+    if (futurPos.i < current_state.length && futurPos.j < current_state.length
+        && futurPos.i >= 0 && futurPos.j >= 0) { // On verifie si toute les conditions sont vraies
+        let oldValue = current_state[futurPos.i][futurPos.j] // On garde notre place precisement dans le tableau
+        current_state[futurPos.i][futurPos.j] = 0 // Puis on la modifie et on la passe à zero pour quelle devienne vide visuellement
+        current_state[ec.i][ec.j] = oldValue // l'ancienne position prend la valeur de la futur visuellement
+        ec.i = futurPos.i;
+        ec.j = futurPos.j; // On redefinie la case vide avec sa nouvelle position
+    }
 }
 
 
 function reset() {
-  setInitState();
-  displayState(current_state);
+    setInitState();
+    displayState(current_state);
 }
 
 // Affichage initial : on fait un reset
